e9449d6aae4d830f75c6396fb294e54ba96c0a8f
Kenneth Li <kennethli.3470@gmail.com>
2016-04-02 20:39:16 -0400

    Cleaned up merge conflicts

--- public/js/game.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview This is the client side script for game.html.
- * @author alvin.lin.dev@gmail.com (Alvin Lin)
- */
-
-$(document).ready(function() {
-  var socket = io();
-
-});
